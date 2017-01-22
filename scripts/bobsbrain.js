6011b97efc5d82a34b2bcb7f18962f85d7ad7c7f
Philippe Charrière <ph.charriere@gmail.com>
2017-01-22 17:10:12 +0100

    Update bobsbrain.js

--- scripts/bobsbrain.js
+++ scripts/bobsbrain.js
@@ -5,6 +5,7 @@
 
 const fetch = require('node-fetch');
 
+console.log("😛", process.env.FROM_GITHUB_HOOK)
 
 module.exports =  (robot) =>  {
 
@@ -17,6 +18,7 @@ module.exports =  (robot) =>  {
   // listen CI server
   // CI server post on this route when it wants to send information to Bob
   robot.router.post(process.env.FROM_GITHUB_HOOK, (req, res) => {
+    console.log("🤖", req.body)
     robot.messageRoom('general', req.body.message)
     res.status(200).end()
   })
